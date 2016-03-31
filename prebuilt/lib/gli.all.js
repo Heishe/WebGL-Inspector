f9fd38e5891a1a26788649389e1ecfecfa8ca1bb
Travis Gesslein <travis.gesslein@pitforest.de>
2016-03-31 03:44:57 +0200

    removed embed.js. now, to include webgl it is simply required to include both the .js or .css of the project in your index.html and then call 'gli.setupContext()' before you create your own webgl context in the application. just do (if(window['gli']) gli.setupContext()) and you're fine

--- prebuilt/lib/gli.all.js
+++ prebuilt/lib/gli.all.js
@@ -774,6 +774,45 @@ function scrollIntoViewIfNeeded(el) {
     };
 
 })();
+(function(){
+    var gli = glinamespace("gli");
+
+    gli.setupContext = function(){
+         // Hook canvas.getContext
+        var originalGetContext = HTMLCanvasElement.prototype.getContext;
+        if (!HTMLCanvasElement.prototype.getContextRaw) {
+            HTMLCanvasElement.prototype.getContextRaw = originalGetContext;
+        }
+        HTMLCanvasElement.prototype.getContext = function () {
+            var ignoreCanvas = this.internalInspectorSurface;
+            if (ignoreCanvas) {
+                return originalGetContext.apply(this, arguments);
+            }
+            
+            var contextNames = ["moz-webgl", "webkit-3d", "experimental-webgl", "webgl"];
+            var requestingWebGL = contextNames.indexOf(arguments[0]) != -1;
+
+            if (requestingWebGL) {
+                // Page is requesting a WebGL context!
+                // TODO: something
+            }
+
+            var result = originalGetContext.apply(this, arguments);
+            if (result == null) {
+                return null;
+            }
+
+            if (requestingWebGL) {
+                // TODO: pull options from somewhere?
+                result = gli.host.inspectContext(this, result);
+                var hostUI = new gli.host.HostUI(result);
+                result.hostUI = hostUI; // just so we can access it later for debugging
+            }
+
+            return result;
+        };
+    }
+})();
 (function () {
     var gli = glinamespace("gli");
 
